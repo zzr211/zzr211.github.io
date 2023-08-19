ef157ad2a7136f9e74d141d1ef66d8dd11ab47f6
zzr211 <zrgood0211@gmail.com>
2023-08-19 13:01:50 +0800

    Site updated: 2023-08-19 13:01:49

--- anzhiyu/random.js
+++
@@ -1,1 +0,0 @@
-var posts=["2022/09/01/上万部免费资源，都在Emby里/","2022/08/09/中国传统色配色网站/","2022/07/09/域名比价与注册/","2022/04/21/英语高频单词表NGSL+NAWL/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
